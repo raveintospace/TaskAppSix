6fdb1b94319d181861f8998ba9960e27f3b375c0
Oriol Angelet <uriaeri@gmail.com>
2025-01-03 18:14:25 +0100

    Initial commit

---
+++ TaskAppSix/TaskAppSix/TaskAppSixApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAppSixApp.swift
+//  TaskAppSix
+//
+//  Created by Uri on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskAppSixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
